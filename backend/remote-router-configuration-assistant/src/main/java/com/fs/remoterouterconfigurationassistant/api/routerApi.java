a14763441ddc96c23fafded6cce64e381c6b95d8
Mayur Kamble <mayur.kamble@forescout.com>
2024-03-07 11:36:28 +0530

    Added extra apis

--- backend/remote-router-configuration-assistant/src/main/java/com/fs/remoterouterconfigurationassistant/api/routerApi.java
+++ backend/remote-router-configuration-assistant/src/main/java/com/fs/remoterouterconfigurationassistant/api/routerApi.java
@@ -1,26 +1,41 @@
 package com.fs.remoterouterconfigurationassistant.api;
 
 import com.fs.remoterouterconfigurationassistant.RouterAccessDetails;
-import org.apache.coyote.Response;
+import com.fs.remoterouterconfigurationassistant.api.model.CommandRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.http.HttpResponse;
-
 @RestController
+@RequestMapping("/api")
 public class routerApi {
 
     @Autowired
     routerApiService routerApiService;
+
     @PostMapping(path = "/connect")
-    public StringBuilder makeSSHConnectionToRouter(@RequestBody RouterAccessDetails accessDetails)
-    {
-        StringBuilder responce = routerApiService.connectToRouter(accessDetails);
+    public String makeSSHConnectionToRouter(@RequestBody RouterAccessDetails accessDetails) {
+        boolean response = routerApiService.connectToRouter(accessDetails);
 
 
-        return  responce;
+        return response ? "CONNECTED" : "CONNECTION_FAILED";
+    }
+
+    @PostMapping(path = "/commands")
+    public String executeCommandOnRouter(@RequestBody CommandRequest commandRequest) {
+
+        String response = routerApiService.executeCommandOnRouter(commandRequest.getCommand());
+
+        System.out.println(response);
+
+        return response;
+    }
+
+
+    @PostMapping(path = "/disconnect")
+    public String disconnectFromRouter() {
+        return routerApiService.disconnectSSH();
     }
 }
