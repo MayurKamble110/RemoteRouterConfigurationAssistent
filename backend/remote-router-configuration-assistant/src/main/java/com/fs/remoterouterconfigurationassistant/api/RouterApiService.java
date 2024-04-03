2b14460b1477223db916d18bb8b4cacbf9b47976
Mayur Kamble <mayur.kamble@forescout.com>
2024-04-03 11:08:53 +0530

    Updated

--- backend/remote-router-configuration-assistant/src/main/java/com/fs/remoterouterconfigurationassistant/api/RouterApiService.java
+++ backend/remote-router-configuration-assistant/src/main/java/com/fs/remoterouterconfigurationassistant/api/RouterApiService.java
@@ -1,11 +1,14 @@
 package com.fs.remoterouterconfigurationassistant.api;
 
 import com.fs.remoterouterconfigurationassistant.RouterAccessDetails;
+import com.fs.remoterouterconfigurationassistant.api.routerCommands.RouterCommandInterpreter;
 import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PutMapping;
 
@@ -18,9 +21,10 @@ import java.util.Arrays;
 public class RouterApiService {
 
     private Session session;
+    @Autowired
+    RouterCommandInterpreter routerCommandInterpreter;
 
     public boolean connectToRouter(RouterAccessDetails accessDetails) {
-
         String username = accessDetails.getUsername();
         String password = accessDetails.getPassword();
         String hostname = accessDetails.getHostIp();
@@ -29,11 +33,8 @@ public class RouterApiService {
             JSch jsch = new JSch();
             session = jsch.getSession(username, hostname, port);
             session.setPassword(password);
-
             session.setConfig("StrictHostKeyChecking", "no");
-
             session.connect();
-
         } catch (JSchException e) {
             System.out.println(e);
             return false;
@@ -42,33 +43,23 @@ public class RouterApiService {
         return true;
     }
 
-    public String executeCommandOnRouter() {
+    public String executeCommandOnRouter(String command) {
         StringBuilder output = new StringBuilder();
         System.out.println(session);
-
         try {
             if (session != null && session.isConnected()) {
                 Channel channel = session.openChannel("exec");
-
-
-                    ((ChannelExec)channel).setCommand("show interfaces && show version");
-
-
-//                channelExec.setCommand(command);
-//                channelExec.setErrStream(System.err);
-
+                ((ChannelExec) channel).setCommand(command);
                 channel.setInputStream(null);
-
-                ((ChannelExec)channel).setErrStream(System.err);
+                ((ChannelExec) channel).setErrStream(System.err);
                 InputStream in = channel.getInputStream();
                 channel.connect();
-
                 byte[] buffer = new byte[1024];
                 int bytesRead;
+
                 while ((bytesRead = in.read(buffer)) != -1) {
                     output.append(new String(buffer, 0, bytesRead));
                 }
-
                 channel.disconnect();
             } else {
                 output.append("SSH session is not established.");
@@ -78,13 +69,12 @@ public class RouterApiService {
             output.append("Error executing command: ").append(e.getMessage());
         }
         System.out.println(output);
+        routerCommandInterpreter.processor(output, command);
 
         return output.toString();
     }
 
-
     public String disconnectSSH() {
-
         if (session == null) {
             return "CONNECTION NOT PRESENT";
         } else if (session != null && session.isConnected()) {
@@ -93,7 +83,5 @@ public class RouterApiService {
         }
 
         return "FAILED TO DISCONNECT FROM ROUTER";
-
-
     }
 }
--- backend/remote-router-configuration-assistant/src/main/java/com/fs/remoterouterconfigurationassistant/databases/ShowInterfaceRepositoryService.java
+++ backend/remote-router-configuration-assistant/src/main/java/com/fs/remoterouterconfigurationassistant/databases/ShowInterfaceRepositoryService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import com.fs.remoterouterconfigurationassistant.api.model.FlaskServerApiRequestBody;
 import com.fs.remoterouterconfigurationassistant.api.model.RouterInterfaceResponceDto;
+import com.fs.remoterouterconfigurationassistant.api.routerCommands.ShowInterfaces;
 import com.fs.remoterouterconfigurationassistant.databases.entities.ShowInterfacesDao;
 import com.fs.remoterouterconfigurationassistant.flaskserver.FlaskServer;
 
@@ -20,20 +21,21 @@ public class ShowInterfaceRepositoryService {
     @Autowired
     private ShowInterfaceRepository repository;
 
-    public void addInterfacesToDatabase(String[] interfaces) {
+    public void addInterfacesToDatabase(String responce) {
+        String[] interfaces = ShowInterfaces.parseData(responce);
+
         for (String data : interfaces) {
             if (data.contains("GigabitEthernet")) {
-                data=data+" output buffers swapped out";
-                RouterInterfaceResponceDto responce =
+                RouterInterfaceResponceDto responceDto =
                                 FlaskServer.makeRequest(new FlaskServerApiRequestBody(data,
                                                 "Give a JSON  object including name,status,ip_address,description and hardware. all these fields should be of string type."));
                 System.out.println("Length : " + data.length());
                 ShowInterfacesDao dao = ShowInterfacesDao.builder()
-                                .status(responce.getStatus())
-                                .name(responce.getName())
-                                .description(responce.getDescription())
-                                .hardware(responce.getHardware())
-                                .ip_address(responce.getIp_address())
+                                .status(responceDto.getStatus())
+                                .name(responceDto.getName())
+                                .description(responceDto.getDescription())
+                                .hardware(responceDto.getHardware())
+                                .ip_address(responceDto.getIp_address())
                                 .raw_information(data)
                                 .build();
                 System.out.println("Inserting a row in database.....");
