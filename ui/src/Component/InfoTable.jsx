4dbccd84df260e30022342adf63ed4ab758428b9
abhikabade <abhishek.kabade@forescout.com>
2024-04-04 13:06:14 +0530

    updated table view

--- ui/src/Component/InfoTable.jsx
+++ ui/src/Component/InfoTable.jsx
@@ -1,81 +1,35 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import DEVICES from '../Data/device';
-import noDataImg from '../Images/noData2.png'
 
-const columns = [
-  { 
-    field: 'id', 
-    headerName: 'ID', 
-    width: 90 },
-  {
-    field: 'name',
-    headerName: 'Name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'ip_address',
-    headerName: 'IP Address',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'status',
-    headerName: 'Status',
-    width: 110,
-    editable: true,
-  },
-  {
-    field: 'hardware',
-    headerName: 'Hardware',
-    description: 'This hardware is used.',
-    sortable: false,
-    width: 160,
-    // valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-  },
-  {
-    field: 'os',
-    headerName: 'OS',
-    width: 110,
-    editable: true,
-  },
-  {
-    field: 'os_version',
-    headerName: 'OS Version',
-    width: 110,
-    editable: true,
-  }
-];
+const VISIBLE_FIELDS = ['id', 'name', 'ip_address', 'status', 'hardware', 'os', 'os_version'];
 
-function NoDataView() {
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center', height: 500, width: '200' }}>
-        <img src={noDataImg} alt="No data available" style={{ width: '50%', height: '50%', paddingTop:'30px' }} /> {/* Adjust width and height as needed */}
-      </div>
-    );
-  }
+export default function DeviceGrid() {
+  const data = React.useMemo(() => {
+    return {
+      rows: DEVICES,
+      columns: VISIBLE_FIELDS.map(field => ({
+        field,
+        headerName: field.replace(/_/g, ' ').toUpperCase(), // Replace underscores with spaces and capitalize
+        width: 130,
+      })),
+    };
+  }, []);
 
-export default function InfoTable() {
   return (
     <Box sx={{ height: 400, width: '100%' }}>
       <DataGrid
-        rows={DEVICES}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: {
-              pageSize: 10,
-            },
+        {...data}
+        disableColumnFilter
+        disableColumnSelector
+        disableDensitySelector
+        slots={{ toolbar: GridToolbar }}
+        slotProps={{
+          toolbar: {
+            showQuickFilter: true,
           },
         }}
-        pageSizeOptions={[10]}
-        // checkboxSelection
-        slots={{ noRowsOverlay: NoDataView}}
-        sx={{ '--DataGrid-overlayHeight': '300px' }}
-        disableRowSelectionOnClick
-        // isCellEditable={(params) => params.row.status === 'up'}
       />
     </Box>
   );
